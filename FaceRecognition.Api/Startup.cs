80f521fb7e998813b106d9e60e429f54ec1c3762
MinhCT <minhct1997@gmail.com>
2018-03-05 09:58:15 +0700

    Comment out unwanted files

--- FaceRecognition.Api/Startup.cs
+++ FaceRecognition.Api/Startup.cs
@@ -12,7 +12,7 @@ namespace FaceRecognition.Api
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
